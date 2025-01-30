329fc327deb25240931c168fd9b6f981dd2eab3c
Jarek Szczupak <dante1802@hotmail.com>
2025-01-30 15:04:49 +0100

    AppsInputData json change

--- Common/KombajnCommon.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Common
-{
-    public static class KombajnCommon
-    {
-        public static string MojeDanePath { get; } = @"C:\Users\dante\Desktop\Istotne\MojeDane";
-        public static string AllDanePath { get; } = @"C:\Users\dante\Desktop\Istotne\All Dane";
-    }
-}
